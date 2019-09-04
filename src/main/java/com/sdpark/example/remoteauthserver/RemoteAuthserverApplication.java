58f3e7f21e75a5bfa710af393dd1d36fe69f1b78
knut-sdpark <sdp1422@naver.com>
2019-09-04 22:19:42 +0900

    first commit

---
+++ src/main/java/com/sdpark/example/remoteauthserver/RemoteAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.sdpark.example.remoteauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteAuthserverApplication.class, args);
+	}
+
+}
